2183411460d6d5ae7ec58411702c0fec38715f59
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2023-10-17 22:02:29 +0100

    last try to change export

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,7 +5,7 @@ import Statistic from 'components/Statistics/Statistic.jsx';
 import Section from 'components/Section/Section.jsx';
 import Notification from 'components/Notification/Notification';
 
-export function App() {
+export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
